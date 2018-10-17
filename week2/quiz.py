b78b14daf565b89f26e7bff737d75da2a1799d2f
RahulD <rahul.sairam@gmail.com>
2018-10-16 20:43:49 -0700

    week2 quiz

---
+++ week2/quiz.py
@@ -0,0 +1,9 @@
+import re
+
+x = 'From: Using the : character'
+y = re.findall('^F.+:', x)
+print(y)
+
+sentence = "From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+y = re.findall("\S+@\S+", sentence)
+print(y)
